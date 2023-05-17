f7fa8ce26835f41ba98ae12c36c5fe2e64f907ce
ermi00 <miguelgonzalez.280803@gmail.com>
2023-05-17 15:29:26 -0600

    Inicio

---
+++ index.js
@@ -0,0 +1,10 @@
+class Alumno{
+    constructor(nombre, apellido, edad, materiasInscritas, calificaciones){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.materiasInscritas = materiasInscritas;
+        this.calificaciones = calificaciones;
+    }
+}
+
