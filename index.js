ad267861ba68fe0a80bd9cd39fe9aba90fa06374
ermi00 <miguelgonzalez.280803@gmail.com>
2023-05-20 15:36:49 -0600

    Funcion de registro alumno y clase

--- index.js
+++ index.js
@@ -8,3 +8,35 @@ class Alumno{
     }
 }
 
+// REGISTRO ALUMNO //
+
+let botonHechoRegistro = document.getElementById("botonHechoRegistro");
+botonHechoRegistro.addEventListener("click", function(){
+
+    let inputNombre = document.getElementById("inputNombre");
+    let inputNombreValor = inputNombre.value;
+
+    let inputApellido = document.getElementById("inputApellido");
+    let inputApellidoValor = inputApellido.value;
+
+    let inputEdad = document.getElementById("inputEdad");
+    let inputEdadValor = inputEdad.value;
+
+    // let nuevoAlumno = new Alumno(inputNombreValor, inputApellidoValor, inputEdadValor);
+
+    let spanRegistroExitoso = document.getElementById("spanRegistroExitoso");
+    spanRegistroExitoso.textContent = `¡Estas Registrado! ${inputNombreValor} ${inputApellidoValor}`
+})
+
+// REGISTRO CLASES //
+
+let botonHechoInscribirClase = document.getElementById("botonHechoInscribirClase");
+botonHechoInscribirClase.addEventListener("click", function(){
+
+    let selectClases = document.getElementById("selectClases");
+    let selectClasesValor = selectClases.value;
+
+    let spanInscripcionClase = document.getElementById("spanInscripcionClase");
+    spanInscripcionClase.textContent = `¡Te has registrado en la clase de ${selectClasesValor}!`
+
+})
